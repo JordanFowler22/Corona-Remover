eaf5453951e647956e80a03227b879b5499d770a
JordanFowler22 <54287603+JordanFowler22@users.noreply.github.com>
2020-04-19 00:47:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+replaceText(document.body)
+
+function replaceText(element) {
+    if (element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText)
+    } else if (element.nodeType === Text.TEXT_NODE) {
+       if (element.textContent.match(/coronavirus/gi)) {
+        element.parentElement.remove()
+       }
+    }
+}
+
+//const newElement = document.createElement('span') 
+        //newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, 
+        //'<span style="background-color: black; color: black;">$1</span')  
+        //element.replaceWith(newElement)
+
+//element.textContent = element.textContent.replace(/coronavirus/gi, '')
+// || element.textContent.match(/COVID-19/gi)

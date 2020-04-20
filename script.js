622ce2a9fc143f523167ffbe40fcdd9d63995d4c
Jordan Fowler <54287603+JordanFowler22@users.noreply.github.com>
2020-04-20 12:31:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
-       if (element.textContent.match(/coronavirus/gi)) {
-        element.parentElement.remove()
-       }
+        if (element.textContent.match(/coronavirus/gi)) {
+            if (element.textContent.match(/COVID-19/gi)) {
+                element.parentElement.remove()
+            }
+            element.parentElement.remove()
+        }
     }
 }
 
 //const newElement = document.createElement('span') 
-        //newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, 
-        //'<span style="background-color: black; color: black;">$1</span')  
-        //element.replaceWith(newElement)
+//newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, 
+//'<span style="background-color: black; color: black;">$1</span')  
+//element.replaceWith(newElement)
 
 //element.textContent = element.textContent.replace(/coronavirus/gi, '')
 // || element.textContent.match(/COVID-19/gi)
